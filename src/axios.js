41b51a2b094e97ab1774f88b0b720a1436059290
santosh <santhosh.t@nuacem.com>
2023-03-10 12:21:11 +0530

    Changed Backend Connection URL

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,11 @@
 import axios from "axios";
+//localhost
+// const instance = axios.create({
+//   baseURL: "http://localhost:9000",
+// });
+
+//Web
 const instance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "https://messaging-app-backend.azurewebsites.net",
 });
 export default instance;
